5b8829fd4c7838d99dcb35cbd1a7aabacf056078
Liv Nelson <livnelson@me.com>
2022-11-01 16:48:26 -0700

    commit tree 4c112f72adc281dd02625d269e7ee9276eef140c

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  const postArray = posts.map((post) => {
+    return (
+      <Article
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+        minutes={post.minutes}
+      />
+    );
+  });
+  return <main>{postArray}</main>;
+}
+
+export default ArticleList;
